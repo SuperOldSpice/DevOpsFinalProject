818d6e97e9f8c3f9aad19d179c2c9c6f77c60e36
SuperOldSpice <71700242+SuperOldSpice@users.noreply.github.com>
2022-02-12 19:10:03 +0200

    Update image_remove.sh

--- image_remove.sh
+++ image_remove.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-let "LAST_BUILD = ${env.BUILD_NUMBER}"
+LAST_BUILD=${env.BUILD_NUMBER}
 let "LAST_BUILD--"
 sudo docker rmi -f $(sudo docker images -q $DOCKERHUB_CREDENTIALS_USR/myapp:$GIT_BRANCH-"$LAST_BUILD"
+EOF
